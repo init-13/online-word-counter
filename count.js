9ddc3db3625d745f0f7852ae7b522d61baad8e2d
Nitin Das <66935434+init-13@users.noreply.github.com>
2021-07-15 16:45:43 +0530

    Update count.js
    dont show special character unless present

--- count.js
+++ count.js
@@ -2,6 +2,7 @@ function counter() {
 		let noc =document.getElementById('input_text').value
 		document.getElementById('displaycount').innerHTML =  noc.length + " Characters "+  noc.trim().split(/\s+/).length + " Words";
 		var format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/gi;
-		document.getElementById('displayspecial').innerHTML = "Special Character "+ noc.match(format).length;
+		if (format.test(noc)){
+		document.getElementById('displayspecial').innerHTML = "Special Character "+ noc.match(format).length;}
 
 	}
