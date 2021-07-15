1cb3e68e3801598311710579b9ed64962497f46d
Nitin Das <66935434+init-13@users.noreply.github.com>
2021-07-15 15:54:51 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,6 +1,9 @@
 function counter() {
 		let noc =document.getElementById('input_text').value
 		document.getElementById('displaycount').innerHTML =  noc.length + " Characters "+  noc.trim().split(/\s+/).length + " Words";
-		document.getElementById('displayspecial').innerHTML = "Special Character "+ (noc.match(/^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g) || []).length;
+	const slen (string) => { let asp='!@#$%^&*()_+{}|:"<>?/.,\';][\\=-'; let count=0; 
+				for (let x of asp){ count+=string.match(x).length;}}
+	
+		document.getElementById('displayspecial').innerHTML = "Special Character "+ slen(noc);
 
 	}
