61099ff687ef7566749595cc23a190c9bd6a7cc6
Nitin Das <66935434+init-13@users.noreply.github.com>
2021-07-15 16:27:58 +0530

    Update count.js

--- count.js
+++ count.js
@@ -1,10 +1,7 @@
 function counter() {
 		let noc =document.getElementById('input_text').value
 		document.getElementById('displaycount').innerHTML =  noc.length + " Characters "+  noc.trim().split(/\s+/).length + " Words";
-	function slen (string) { let asp='!@#$%^&*()_+{}|:"<>?/.,\';][\\=-'; let count=0; 
-				for (let x of asp){ count+=string.match(x).length;}
-			       return count;}
 	
-		document.getElementById('displayspecial').innerHTML = "Special Character "+ slen(noc);
+		document.getElementById('displayspecial').innerHTML = "Special Character "+ noc.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/gi).length;
 
 	}
